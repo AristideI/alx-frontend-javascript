54afa75f2a8debe96ae728748e9a25ecbe920a80
AristideI <115247039+AristideI@users.noreply.github.com>
2023-09-25 14:58:11 +0200

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,12 +3,6 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  if (trueOrFalse) {
-    task = false;
-    task2 = true;
-   
-  }
-
   return [task, task2];
 }
 
